0bb8c4dce9226bd759915333d7ab98643cf2e0c1
slavkoder <slava.panasenko@quizlet.com>
2019-07-03 19:06:41 -0700

    Add debugging lab solution

---
+++ lab1-debugging/app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
@@ -0,0 +1,64 @@
+package com.codepath.debuggingchallenges.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.codepath.debuggingchallenges.R;
+import com.codepath.debuggingchallenges.adapters.MoviesAdapter;
+import com.codepath.debuggingchallenges.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    RecyclerView rvMovies;
+    MoviesAdapter adapter;
+    ArrayList<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+        rvMovies = findViewById(R.id.rvMovies);
+
+        // Set layout manager to tell RecyclerView how to display items
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        fetchMovies();
+    }
+
+
+    private void fetchMovies() {
+        String url = " https://api.themoviedb.org/3/movie/now_playing?api_key=" + API_KEY;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray moviesJson = response.getJSONArray("results");
+                    movies = Movie.fromJSONArray(moviesJson);
+
+                    // Create the adapter to convert the array to views
+                    adapter = new MoviesAdapter(movies);
+
+                    // Attach the adapter to a RecyclerView
+                    rvMovies.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
